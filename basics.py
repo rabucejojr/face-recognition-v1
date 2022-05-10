99b37071cc80ae18b872fa71b8f3922baa59d141
rabucejo <rogerhapay@gmail.com>
2022-05-10 21:39:49 +0800

    initial upload

---
+++ basics.py
@@ -0,0 +1,59 @@
+from base64 import encode
+import cv2
+from cv2 import resize
+import face_recognition
+
+import cv2
+import numpy as np
+import face_recognition
+import os
+
+path = 'attendance'
+images = []
+classNames = []
+list = os.listdir(path)
+
+for cls in list:
+    curImg = cv2.imread(f'{path}/{cls}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cls)[0])
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    imgS = cv2.resize(img,)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    currentFaceLoc = face_recognition.face_locations(imgS)[0]
+    encodeCurrentFace = face_recognition.face_encodings(imgS, currentFaceLoc)
+
+    for encodeFace, faceLoc in zip(encodeCurrentFace, currentFaceLoc):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        matchIndex = np.argmin(faceDis)
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(img, (x1, y2 - 35), (x2, y2),
+                          (0, 255, 0), cv2.FILLED)
+            cv2.rectangle(img, name, (x1+6, y2-6),
+                          cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow('webcam', img)
+    cv2.waitKey(1)
